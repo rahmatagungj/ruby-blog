1e3d6dbf61573210766f1158921e5c4aa0e3d82d
rahmatagungj <rahmatagungj@gmail.com>
2021-03-18 01:01:04 +0700

    fix PWA

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       // console.log("Cached offline page during install");
 
-      if (offlineFallbackPage === "index.html") {
+      if (offlineFallbackPage === "to-do.html") {
         return cache.add(new Response("Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
 
