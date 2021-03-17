45b2a4504855c2644e9a2191dd3edecc4df1ab03
rahmatagungj <rahmatagungj@gmail.com>
2021-03-17 12:58:12 +0700

    fix PWA Support

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,9 +2,14 @@
 layout: null
 ---
 window.onload = function () {
-    var script = document.createElement('script');
-    var firstScript = document.getElementsByTagName('script')[0];
-    script.async = true;
-    script.src = '{{'sw-register.js'|relative_url}}?v=' + Date.now();
-    firstScript.parentNode.insertBefore(script, firstScript);
+    if ('serviceWorker' in navigator) {
+        var script = document.createElement('script');
+        var firstScript = document.getElementsByTagName('script')[0];
+        script.async = true;
+        script.src = '{{'sw-register.js'|relative_url}}?v=' + Date.now();
+        firstScript.parentNode.insertBefore(script, firstScript);
+    }
+    else{
+        console.log('Service Workers not supported');
+    }
 };
