ae1d6320f55881dc9794970035d267209635c0d3
rahmatagungj <rahmatagungj@gmail.com>
2021-03-24 14:56:43 +0700

    remove unused css

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -34,26 +34,37 @@ document.addEventListener("turbolinks:load", function () {
 });
 
 function a2hs() {
+  // ADD TO HOMESCREEN
+  let deferredPrompt;
+
+  const addBtn = document.getElementById("addButton");
+
+  addBtn.style.display = "none";
+
   window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
+    // Stash the event so it can be triggered later.
     deferredPrompt = e;
-  });
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = "block";
 
-  const btnInstallApp = document.getElementById("addButton");
-
-  if (btnInstallApp) {
-    btnInstallApp.addEventListener("click", (e) => {
+    addBtn.addEventListener("click", (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = "none";
+      // Show the prompt
       deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === "accepted") {
-          console.log("user accepted A2HS prompt");
+          console.log("User accepted the A2HS prompt");
         } else {
-          console.log("user dismissed A2HS prompt");
+          console.log("User dismissed the A2HS prompt");
         }
         deferredPrompt = null;
       });
     });
-  }
+  });
 }
 
 function networkNotification() {
