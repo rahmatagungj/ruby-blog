e7d6722d1cb3241c1161b09e22a8941fa9086016
rahmatagungj <rahmatagungj@gmail.com>
2021-04-04 22:39:49 +0700

    :boom: Add new CI Python build and run

--- build/run.py
+++ build/run.py
@@ -1,9 +1,4 @@
 import os
 
-try:
-    import minifier
-except:
-    print("Skipping minifier ... ")
-
 print("\n== RUNNING SERVER ")
-os.system("cd .. && webpack build && sudo bundle exec jekyll serve --livereload -w")
+os.system("cd .. && sudo bundle exec jekyll serve --livereload -w")
--- build/run_dev.py
+++ build/run_dev.py
@@ -1,10 +1,5 @@
 import os
 
-try:
-    import minifier
-except:
-    print("Skipping minifier ... ")
-
 print("\n== RUNNING SERVER ")
 os.system(
     "cd .. && webpack build && sudo bundle exec jekyll serve --limit_posts 3 --livereload -w"
